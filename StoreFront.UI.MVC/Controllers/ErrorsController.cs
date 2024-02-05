0be3c9bf91c06a35ade4fe5eff4b63c4461a3aca
npark080 <npark080@gmail.com>
2024-02-04 22:27:01 -0600

    Added custom error handling

---
+++ StoreFront.UI.MVC/Controllers/ErrorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CORE3.UI.MVC.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult Status(int id)
+        {
+            (int id, string message) error;
+            
+            error.id = id;
+            error.message = id switch
+            {
+                404 => "Page Not Found",
+                400 => "Bad Request",
+                500 => "Internal Server Error",
+                505 => "Access Denied",
+                _ => "Unknown Error"
+            }; 
+
+            return View(error);
+        }
+    }
+}
